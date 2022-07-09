ac58f03cb23e959c823d1af312c2055f02e2b304
Cleidson Estevam <cleidsonestevamdasilva@hotmail.com>
2022-07-08 22:57:04 -0300

    GeekShoppingMicrosservices

---
+++ Geek_Shopping/GeekShopping.ProductAPI/Repository/Interface/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.ProductAPI.Data.DTO;
+
+namespace GeekShopping.ProductAPI.Repository.Interface
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> FindAll();
+        Task<ProductDTO> FindById(long id);
+        Task<ProductDTO> Create(ProductDTO prod);
+        Task<ProductDTO> Update(ProductDTO prod);
+        Task<bool> Delete(long id); 
+    }
+}
