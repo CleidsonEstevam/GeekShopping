518ee8380f9439ebdc08aa89b4b3c1b142d47407
Cleidson Estevam <cleidsonestevamdasilva@hotmail.com>
2022-07-13 19:43:20 -0300

    Adicionando HttpClient no front.

---
+++ Geek_Shopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel prod);
+        Task<ProductModel> UpdateProduct(ProductModel prod);
+        Task<bool> DeleteProduct(long id);
+    }
+}
