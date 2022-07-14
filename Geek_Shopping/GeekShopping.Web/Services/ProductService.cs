d9b8df4d027ec3e844eead7a1dbb0d2faf577b49
Cleidson Estevam <cleidsonestevamdasilva@hotmail.com>
2022-07-14 15:35:25 -0300

    Adicionado metodos de pesquisa do httpClient

---
+++ Geek_Shopping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Utils;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        public const string BasePath = "api/v1/product";
+        public async Task<IEnumerable<ProductModel>> FindAllProducts()
+        {
+            var response = await _httpClient.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindProductById(long id)
+        {
+            var response = await _httpClient.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+        public async Task<ProductModel> CreateProduct(ProductModel prod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductModel> UpdateProduct(ProductModel prod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteProduct(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
