a10b635455a3090692b28216685b3fad620fae10
Cleidson Estevam <cleidsonestevamdasilva@hotmail.com>
2022-07-27 16:03:02 -0300

    Adicionado context no Identity Server.

---
+++ Geek_Shopping/GeekShopping.IdentityServer/Model/MySqlContext.cs
@@ -0,0 +1,11 @@
+﻿using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.IdentityServer.Model
+{
+    public class MySqlContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+    }
+}
