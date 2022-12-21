2e5f829bed77c82c26701a9f68b68cd9a2752130
Cleidson Estevam Da Silva <cleidson.silva@reval.corp>
2022-12-21 14:52:56 -0300

    CarrinhoFinalizado

---
+++ Geek_Shopping/GeekShopping.Web/Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await FindUserCart());
+        }
+
+        public async Task<IActionResult> Remove(int id) 
+        {
+            //Pegar Token e UserId
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            //Carregar delete no response
+            var response = await _cartService.RemoveFromCart(id, token);
+            if (response)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        private async Task<CartViewModel> FindUserCart() 
+        {
+            //Pegar Token e UserId
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            //Ir buscar response com dados do carrinho 
+            var response = await _cartService.FindCartByUserId(userId, token);
+
+            //Somar itens e adicionar total
+            if (response?.CartHeader != null) 
+            {
+                foreach (var detail in response.CartDetails) 
+                {
+                    response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+                }   
+            }
+            return response;
+        }
+
+    }
+}
