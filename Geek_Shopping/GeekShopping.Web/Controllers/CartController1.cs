8440bcb9f0bd08980dff5eb31c04b41d5c2803e9
Cleidson Estevam Da Silva <cleidson.silva@reval.corp>
2022-12-21 10:39:07 -0300

    InicioCartController

---
+++ Geek_Shopping/GeekShopping.Web/Controllers/CartController1.cs
@@ -0,0 +1,23 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class CartController1 : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController1(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+    }
+}
